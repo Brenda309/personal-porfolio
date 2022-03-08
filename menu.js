2b6832e6ff4bd3e40c9cca0cb245a11c21a34b74
amrendrakind <vision2010@gmail.com>
2022-03-08 20:02:58 +0530

    JS Linter and menu.js updated.

--- menu.js
+++ menu.js
@@ -2,6 +2,20 @@ const navbar = document.querySelector('.navbar');
 const menu = document.querySelector('.menu');
 const cancel = document.querySelector('.cancel');
 
+menu.addEventListener('click', () => {
+    menu.style.display = 'none';
+    document.querySelector('.logo').style.display = 'none';
+
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.style.display = 'flex';
+    navMenu.style.flexDirection = 'column';
+    navMenu.style.listStyle = 'none';
+    navMenu.style.textDecoration = 'none';
+
+
+})
+
+
 menu.addEventListener('click', );
 cancel.addEventListener('click', close);
 
