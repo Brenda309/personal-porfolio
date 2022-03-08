b5eddebd73de96e975db029da108fe84b1a91bab
Brenda309 <brewihogora23@gmail.com>
2022-03-08 23:20:37 +0200

    Add the menu back to the desktop-version

--- menu.js
+++ menu.js
@@ -1,26 +1,23 @@
-const navbar = document.querySelector('.navbar');
+
 const menu = document.querySelector('.menu');
-const cancel = document.querySelector('.cancel');
 const navMenu = document.querySelector('.nav-menu');
 
 menu.addEventListener('click', () => {
-    menu.style.display = 'none';
-    document.querySelector('.logo').style.display = 'none';
-    const navMenu = document.querySelector('.nav-menu');
-    const listItems = document.getElementById('list-items');
-    navMenu.style.display = 'flex';
-    navMenu.style.flexDirection = 'column';
-    navMenu.style.listStyle = 'none';
-    navMenu.style.textDecoration = 'none';
+  menu.style.display = 'none';
+  document.querySelector('.logo').style.display = 'none';
+  const navMenu = document.querySelector('.nav-menu');
+  const listItems = document.getElementById('list-items');
+  navMenu.style.display = 'flex';
+  navMenu.style.flexDirection = 'column';
+  navMenu.style.listStyle = 'none';
+  navMenu.style.textDecoration = 'none';
 
-    navMenu.classList.add('menu-item-style');
+  navMenu.classList.add('menu-item-style');
   listItems.classList.add('list-items');
-
-})
+});
 
 navMenu.addEventListener('click', () => {
-  
-  document.querySelector('.nav-menu').style.display = 'none'
+  document.querySelector('.nav-menu').style.display = 'none';
   document.querySelector('.logo').style.display = 'flex';
-  menu.style.display = 'flex'; 
+  menu.style.display = 'flex';
 });
