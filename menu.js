8486cfa78b1cfd4d1c71d8f319765c789cac0e6b
Brenda309 <brewihogora23@gmail.com>
2022-03-08 16:16:47 +0200

    Add the js file

---
+++ menu.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector('.navbar');
+const menu = document.querySelector('.menu');
+const cancel = document.querySelector('.cancel');
+
+menu.addEventListener('click', );
+cancel.addEventListener('click', close);
+
+
+function show() {
+  navbar.style.display = 'flex';
+}
