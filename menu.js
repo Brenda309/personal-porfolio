a3f7591925e4d4a58947f24b8b4c31eeab277a16
Brenda309 <brewihogora23@gmail.com>
2022-03-12 19:51:05 +0200

    Add the html dynamicaly

--- menu.js
+++ menu.js
@@ -72,144 +72,88 @@ const popupObject = [
   },
 ];
 
+// Creating PopupWindow
+let workSectionContainer;
+let popupWindowCont;
+function popupActive(h1, subworkli1, subworkli2, subworkli3, popupImg,
+  descriptionT, technolist1, technolist2, technolist3, technolist4, technolist5,
+  technolist6, githublink) {
+  popupWindowCont = document.createElement('section');
+  workSectionContainer.appendChild(popupWindowCont);
+  popupWindowCont.className = 'popupWindowCont';
+  popupWindowCont.innerHTML = `
+    <div class="popupWindow">
+      <img src="images/xicon.png" class="popupCloseicon">
+      <h2 class="p-title">${h1}</h2>
+      <ul class="subwork">
+        <li>${subworkli1}</li>
+        <li>${subworkli2}</li>
+        <li>${subworkli3}</li>
+      </ul>
+      <img src="${popupImg}" class="featuredImage">
+      <div id="infoCont">
+        <p>${descriptionT}</p>
+        <div id="lang-btn-cont">
+          <ul class="languages">
+            <li>${technolist1}</li>
+            <li>${technolist2}</li>
+            <li>${technolist3}</li>
+            <li class="hidden-lists">${technolist4}</li>
+            <li class="hidden-lists">${technolist5}</li>
+            <li class="hidden-lists">${technolist6}</li>
+          </ul>
+          <hr id="popuphr">
+          <div id="btn-Cont">
+            <a href="" class="seeLive">See live<img src="images/livecode.png"></a>
+            <a href="${githublink}" target="_blank" class="seeSrc">See source<img src="images/bluegithub.png"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+  const closePopup = document.querySelector('.popupCloseicon');
+  closePopup.addEventListener('click', () => {
+    workSectionContainer.removeChild(popupWindowCont);
+  });
+}
 // Creating Main section dynamically
-const workSectionContainer = document.querySelector('.cards-container');
-for (let i = 0; i < 4; i += 1) {
+workSectionContainer = document.querySelector('.cards-container');
+for (let i = 0; i < popupObject.length; i += 1) {
   const card = document.createElement('section');
   card.className = popupObject[i].cardClass;
-  const cardSection = document.createElement('div');
-  cardSection.className = 'image';
   workSectionContainer.appendChild(card);
-  card.appendChild(cardSection);
-  const featuredImage = document.createElement('img');
-  featuredImage.className = 'featuredImage';
-  featuredImage.src = popupObject[i].image1;
-  cardSection.appendChild(featuredImage);
-  const bgdiv = document.createElement('div');
-  bgdiv.className = 'background';
-  card.appendChild(bgdiv);
-  const h2 = document.createElement('h2');
-  h2.className = 'p-title';
-  h2.textContent = popupObject[i].name;
-  bgdiv.appendChild(h2);
-  const bgdivUl = document.createElement('ul');
-  bgdivUl.className = 'subwork';
-  bgdiv.appendChild(bgdivUl);
-  const bgdivUlLi1 = document.createElement('li');
-  const bgdivUlLi2 = document.createElement('li');
-  const bgdivUlLi3 = document.createElement('li');
-  bgdivUl.append(bgdivUlLi1, bgdivUlLi2, bgdivUlLi3);
-  bgdivUlLi1.className = 'work1';
-  const subwork1index = popupObject[i].subwork[0];
-  const subwork2index = popupObject[i].subwork[1];
-  const subwork3index = popupObject[i].subwork[2];
-  bgdivUlLi1.textContent = subwork1index;
-  bgdivUlLi2.textContent = subwork2index;
-  bgdivUlLi3.textContent = subwork3index;
-  const bgdivP = document.createElement('p');
-  bgdivP.textContent = popupObject[i].description;
-  bgdiv.appendChild(bgdivP);
-  bgdivP.className = 'about';
-  const languagesUl = document.createElement('ul');
-  languagesUl.className = 'languages';
-  bgdiv.appendChild(languagesUl);
-  const languagesUlLi1 = document.createElement('li');
-  const technologiesindex1 = popupObject[i].technologies[0];
-  languagesUlLi1.textContent = technologiesindex1;
-  const languagesUlLi2 = document.createElement('li');
-  const technologiesindex2 = popupObject[i].technologies[1];
-  languagesUlLi2.textContent = technologiesindex2;
-  const languagesUlLi3 = document.createElement('li');
-  const technologiesindex3 = popupObject[i].technologies[2];
-  languagesUlLi3.textContent = technologiesindex3;
-
-  const languagesUlLi4 = document.createElement('li');
-  const languagesUlLi5index1 = popupObject[i].technologies[3];
-  languagesUlLi4.textContent = languagesUlLi5index1;
-  const languagesUlLi5 = document.createElement('li');
-  const languagesUlLi5index2 = popupObject[i].technologies[4];
-  languagesUlLi5.textContent = languagesUlLi5index2;
-  const languagesUlLi6 = document.createElement('li');
-  const languagesUlLi6index3 = popupObject[i].technologies[5];
-  languagesUlLi6.textContent = languagesUlLi6index3;
-  languagesUl.append(languagesUlLi1, languagesUlLi2, languagesUlLi3);
-  languagesUl.append(languagesUlLi4, languagesUlLi5, languagesUlLi6);
-  languagesUlLi4.style.display = 'none';
-  languagesUlLi5.style.display = 'none';
-  languagesUlLi6.style.display = 'none';
-  const btnDiv = document.createElement('div');
-  bgdiv.appendChild(btnDiv);
+  card.innerHTML = `
+  <div class="image">
+    <img class="featuredImage" src="${popupObject[i].image1}">
+  </div>
+  <div class="background">
+    <h2 class="p-title">${popupObject[i].name}</h2>
+    <ul class="subwork">
+      <li class="work1">${popupObject[i].subwork[0]}</li>
+      <li>${popupObject[i].subwork[1]}</li>
+      <li>${popupObject[i].subwork[2]}</li>
+    </ul>
+    <p class="about">${popupObject[i].description}</p>
+    <ul class="languages">
+      <li>${popupObject[i].technologies[0]}</li>
+      <li>${popupObject[i].technologies[1]}</li>
+      <li>${popupObject[i].technologies[2]}</li>
+    </ul>
+    <div class="btnDiv"></div>
+  </div>
+  `;
   const btn = document.createElement('button');
+  const btnDiv = document.querySelectorAll('.btnDiv');
   btn.className = 'clickme';
-  btnDiv.appendChild(btn);
+  btnDiv.forEach((element) => {
+    element.appendChild(btn);
+  });
   btn.textContent = 'See Projects';
   btn.addEventListener('click', () => {
-    if (window.screen.width > 992) {
-      languagesUlLi4.style.display = 'inline';
-      languagesUlLi5.style.display = 'inline';
-      languagesUlLi6.style.display = 'inline';
-    }
-    const popupWindowMainCont = document.createElement('section');
-    workSectionContainer.appendChild(popupWindowMainCont);
-    popupWindowMainCont.className = 'popupWindowCont';
-    const popupwindow = document.createElement('section');
-    popupWindowMainCont.appendChild(popupwindow);
-    popupwindow.className = 'popupWindow';
-    const closeIcon = document.createElement('img');
-    closeIcon.src = 'images/xIcon.svg';
-    closeIcon.style.float = 'right';
-    popupwindow.appendChild(closeIcon);
-    closeIcon.addEventListener('click', () => {
-      languagesUlLi4.style.display = 'none';
-      languagesUlLi5.style.display = 'none';
-      languagesUlLi6.style.display = 'none';
-      popupWindowMainCont.style.display = 'none';
-      bgdiv.appendChild(h2);
-      bgdiv.appendChild(bgdivUl);
-      cardSection.appendChild(featuredImage);
-      bgdiv.appendChild(bgdivP);
-      bgdivP.textContent = popupObject[i].description;
-      bgdiv.appendChild(languagesUl);
-      bgdiv.appendChild(btn);
-      btn.style.width = '130px';
-      languagesUl.style.marginBottom = '0';
-    });
-    popupwindow.appendChild(h2);
-    popupwindow.appendChild(bgdivUl);
-    popupwindow.appendChild(featuredImage);
-    const pDivider = document.createElement('div');
-    pDivider.className = 'pDivider';
-    popupwindow.appendChild(pDivider);
-    pDivider.appendChild(bgdivP);
-    bgdivP.textContent = popupObject[i].popupDescription;
-    const techDiv = document.createElement('div');
-    techDiv.className = 'techDiv';
-    pDivider.appendChild(techDiv);
-    techDiv.appendChild(languagesUl);
-    const popupbtn1 = document.createElement('a');
-    const popupbtn2 = document.createElement('a');
-    const popupbtnDiv = document.createElement('div');
-    techDiv.appendChild(popupbtnDiv);
-    popupbtn1.className = 'popupClickme1';
-    popupbtn2.className = 'popupClickme2';
-    popupbtn1.textContent = 'See live';
-    popupbtn2.textContent = 'See source';
-    const icon1 = document.createElement('img');
-    const icon2 = document.createElement('img');
-    icon2.src = 'images/githubinbutton.svg';
-    popupbtn2.appendChild(icon2);
-    icon1.src = 'images/roundShape.png';
-    icon1.style.marginBottom = '-3px';
-    icon2.style.margin = '0 5px -3px 6px';
-    icon1.style.margin = '0 5px -3px 6px';
-    popupbtn1.appendChild(icon1);
-    popupbtn2.href = popupObject[i].secodbutton;
-    popupbtnDiv.append(popupbtn1, popupbtn2);
-    const hr = document.createElement('hr');
-    hr.className = 'hr';
-    techDiv.insertBefore(hr, popupbtnDiv);
-    hr.style.color = '#ebecf0';
-    popupbtnDiv.className = 'popupBtnDiv';
-    languagesUl.style.marginBottom = '20px';
+    popupActive(popupObject[i].name, popupObject[i].subwork[0], popupObject[i].subwork[1],
+      popupObject[i].subwork[2], popupObject[i].image1, popupObject[i].description,
+      popupObject[i].technologies[0], popupObject[i].technologies[1],
+      popupObject[i].technologies[2], popupObject[i].technologies[3],
+      popupObject[i].technologies[4], popupObject[i].technologies[5], popupObject[i].secodbutton);
   });
 }
